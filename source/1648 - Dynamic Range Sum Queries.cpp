3b53529f1aad509db2c54927cd557796a583cb36
Cao Trung Đức <103615767+TrungDuc19@users.noreply.github.com>
2022-08-11 16:02:49 +0700

    Create 1648 - Dynamic Range Sum Queries.cpp

---
+++ source/1648 - Dynamic Range Sum Queries.cpp
@@ -0,0 +1,60 @@
+/* Segment Tree */
+#include <iostream>
+using namespace std;
+const int N = 2e5 + 5;
+int n, q, pos, val, l, r;
+long long arr[N], STree[4*N];
+void build(int id, int u, int v)
+{
+    if (u == v) {
+        STree[id] = arr[u];
+        return;
+    }
+    int m = (u + v)/2;
+    build(2*id, u, m);
+    build(2*id+1, m+1, v);
+    STree[id] = STree[2*id] + STree[2*id+1];
+}
+void update(int id, int u, int v)
+{
+    if (u > pos || v < pos) return;
+    if (u == v) {
+        STree[id] = val;
+        return;
+    }
+    int m = (u + v)/2;
+    update(2*id, u, m);
+    update(2*id+1, m+1, v);
+    STree[id] = STree[2*id] + STree[2*id+1];
+}
+long long get(int id, int u, int v)
+{
+    if (v < l || u > r) return 0;
+    if (l <= u && v <= r) {
+        return STree[id];
+    }
+    int m = (u + v)/2;
+    return (get(2*id, u, m) + get(2*id+1, m+1, v));
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int t = 1; //cin >> t;
+    while (t--) {
+        cin >> n >> q;
+        for (int i = 1; i <= n; i++) cin >> arr[i];
+        build(1, 1, n);
+        while (q--) {
+            int choose; cin >> choose;
+            if (choose == 1) {
+                cin >> pos >> val;
+                update(1, 1, n);
+            }
+            else {
+                cin >> l >> r;
+                cout << get(1, 1, n) << '\n';
+            }
+        }
+    }
+    return 0;
+}
