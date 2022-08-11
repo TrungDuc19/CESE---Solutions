b707d9c727bb8577e129dab2be95fa4b3d0f7071
Cao Trung Đức <103615767+TrungDuc19@users.noreply.github.com>
2022-08-11 16:01:16 +0700

    Update 1647 - Static Range Minimum Queries.cpp

--- source/1647 - Static Range Minimum Queries.cpp
+++ source/1647 - Static Range Minimum Queries.cpp
@@ -1,3 +1,4 @@
+/* Sparse Table */
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -34,3 +35,48 @@ int main()
     }
 	return 0;
 }
+
+
+
+/* Segment Tree */
+#include <iostream>
+using namespace std;
+const int N = 2e5 + 5;
+int n, q, l, r;
+int arr[N], STree[4*N];
+void build(int id, int u, int v)
+{
+    if (u == v) {
+        STree[id] = arr[u];
+        return;
+    }
+    int m = (u + v)/2;
+    build(2*id, u, m);
+    build(2*id+1, m+1, v);
+    STree[id] = min(STree[2*id], STree[2*id+1]);
+}
+int get(int id, int u, int v)
+{
+    if (v < l || u > r) return 2e9;
+    if (l <= u && v <= r) {
+        return STree[id];
+    }
+    int m = (u + v)/2;
+    return min(get(2*id, u, m), get(2*id+1, m+1, v));
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int t = 1; //cin >> t;
+    while (t--) {
+        cin >> n >> q;
+        for (int i = 1; i <= n; i++) cin >> arr[i];
+        build(1, 1, n);
+        while (q--) {
+            cin >> l >> r;
+            cout << get(1, 1, n) << '\n';
+        }
+    }
+    return 0;
+}
+//
